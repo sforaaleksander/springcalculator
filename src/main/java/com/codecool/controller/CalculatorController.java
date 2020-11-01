18b1249fe1fd99a9e7ad7e3ed4cf3573d8cf4a97
sforaaleksander <sforaaleksander@gmail.com>
2020-11-01 14:59:14 +0100

    implement calculating and display

--- src/main/java/com/codecool/controller/CalculatorController.java
+++ src/main/java/com/codecool/controller/CalculatorController.java
@@ -1,6 +1,8 @@
 package com.codecool.controller;
 
+import com.codecool.model.Calculus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,5 +14,11 @@ public class CalculatorController {
     @RequestMapping(value = "/calculator", method = RequestMethod.GET)
     public String calculator(HttpServletRequest request){
         return "form";
+    }
+
+    @RequestMapping(value = "/calculator", method = RequestMethod.POST)
+    public String calculate(HttpServletRequest request, Model model){
+        model.addAttribute("result", Calculus.calculate(request));
+        return "result";
     }
 }
--- src/main/java/com/codecool/model/Calculus.java
+++ src/main/java/com/codecool/model/Calculus.java
@@ -1,8 +1,26 @@
 package com.codecool.model;
 
-public class Calculus {
+import javax.servlet.http.HttpServletRequest;
 
-    public static String getResult(String result) {
-        return "the result is " + result;
+public class Calculus {
+    public static String calculate(HttpServletRequest request) {
+        int no1 = Integer.parseInt(request.getParameter("no1"));
+        int no2 = Integer.parseInt(request.getParameter("no2"));
+        int result = 0;
+        switch (request.getParameter("operator")) {
+            case "add":
+                result = no1 + no2;
+                break;
+            case "subtract":
+                result = no1 - no2;
+                break;
+            case "multiply":
+                result = no1 * no2;
+                break;
+            case "divide":
+                result = no1 / no2;
+                break;
+        }
+        return "The result is " + result;
     }
 }
